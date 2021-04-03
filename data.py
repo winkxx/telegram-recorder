e1ab88e0898e73b6a1a94408c099c4c6d2496e7e
winkxx <68945988+winkxx@users.noreply.github.com>
2021-04-03 13:57:22 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
-api_id = 0 # API ID
-api_hash = 'Access Hash'
+api_id = 2948911 # API ID
+api_hash = 'd694de0c87f7bee3ecc57da8d0eda7ea'
 
 listened_groups_id = [
-
+1306294464,1126205241,1448826693,1328088425
 ]
 
-listened_persons_id = [
+listened_persons_id = []
 
+fwd_channel = [
+1331789615
 ]
-
-fwd_channel = []
